f924771c02b3d3f510106b66854bde2add28841c
Kenny Claka <kennyigbechi@gmail.com>
2019-10-26 10:05:47 -0700

    finished all methods

--- src/com/netBank/org/BankProgram.java
+++ src/com/netBank/org/BankProgram.java
@@ -2,6 +2,7 @@ package com.netBank.org;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 public class BankProgram {
 
@@ -17,7 +18,7 @@ public class BankProgram {
     public void run(){
         scanner = new Scanner(System.in);
         while(!done){
-            System.out.print("Enter Command (0 = quit, 1 = new, 2 = select, 3 = deposit, 4 = loan, 4 = show, 6 = interest) ");
+            System.out.print("Enter Command (0 = quit, 1 = new, 2 = select, 3 = deposit, 4 = loan, 5 = show, 6 = interest) ");
             int cmd = scanner.nextInt();
             processCommand(cmd);
         }
@@ -35,4 +36,59 @@ public class BankProgram {
             System.out.println("Illegal command");
 
     }
+
+    private void quit(){
+        done = true;
+        System.out.println("Thank You");
+    }
+
+    private void newAccount(){
+        current = nextacct++;
+        accounts.put(current, 0);
+        System.out.println("Your new Account number is" + current);
+    }
+
+    private void select(){
+        System.out.print("Enter Account#: ");
+        current = scanner.nextInt();
+        int balance = accounts.get(current);
+        System.out.println("The balance of account " + current + "is " + balance);
+    }
+
+    private void deposit(){
+        System.out.print("Enter deposit amount: ");
+        int amt = scanner.nextInt();
+        int balance = accounts.get(current);
+        accounts.put(current, balance+amt);
+    }
+
+    private void authorizeLoan(){
+        System.out.println("Enter Loan Amount: ");
+        int loanamt = scanner.nextInt();
+        int balance = accounts.get(current);
+        if (balance >= loanamt/2)
+            System.out.println("Your loan is approved");
+        else
+            System.out.println("Your Loan is denied");
+    }
+
+    private void showAll(){
+        Set<Integer> accts = accounts.keySet();
+        System.out.println("The bank has " + accts.size()
+        + "accounts");
+
+        for(int i : accts)
+            System.out.println("\tBank account" + i
+            + ": balance=" + accounts.get(i));
+    }
+
+    private void addInterest(){
+        Set<Integer> accts = accounts.keySet();
+        for(int i : accts){
+            int balance = accounts.get(i);
+            int newbalance = (int) (balance * (1 + rate));
+            accounts.put(i, newbalance);
+        }
+    }
+
 }
